65877d462da55cd7f7cf2d6b54428a31d2ace646
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2022-01-12 17:49:31 +0200

    full blown flow 3

--- upload.js
+++ upload.js
@@ -1,12 +1,15 @@
-const { create } = require('@actions/artifact');
-const core = require('@actions/core');
-const path = require('path');
-const fs = require('fs-extra');
+const { create } = require("@actions/artifact");
+const {
+  DownloadHttpClient,
+} = require("@actions/artifact/lib/internal/download-http-client");
+const core = require("@actions/core");
+const path = require("path");
+const fs = require("fs-extra");
 
 async function upload() {
   const tempDir = path.join(
-    process.env['RUNNER_TEMP'] || __dirname,
-    'turbo-cache'
+    process.env["RUNNER_TEMP"] || __dirname,
+    "turbo-cache"
   );
 
   fs.ensureDirSync(tempDir);
@@ -15,27 +18,38 @@ async function upload() {
 
   const files = fs.readdirSync(tempDir);
 
-  const artifactFiles = files.filter((filename) => filename.endsWith('.gz'));
+  const artifactFiles = files.filter((filename) => filename.endsWith(".gz"));
 
   core.debug(`artifact files: ${JSON.stringify(artifactFiles, null, 2)}`);
 
+  const downloadHttpClient = new DownloadHttpClient();
+  const artifacts = await downloadHttpClient.listArtifacts();
+  const existingArtifacts = artifacts.value.map((artifact) => artifact.name);
+
+  core.info(`existing artifacts ${JSON.stringify(existingArtifacts)}`);
+
   await Promise.all(
-    artifactFiles.map(async (artifactFilename) => {
-      const filenameWithoutExt = path.basename(
-        artifactFilename,
-        path.extname(artifactFilename)
-      );
+    artifactFiles
+      .map((artifactFilename) => {
+        const artifactId = path.basename(
+          artifactFilename,
+          path.extname(artifactFilename)
+        );
 
-      core.info(`Uploading ${artifactFilename}`);
+        return { artifactFilename, artifactId };
+      })
+      .filter(({ artifactId }) => !existingArtifacts.includes(artifactId))
+      .map(async ({ artifactFilename, artifactId }) => {
+        core.info(`Uploading ${artifactFilename}`);
 
-      await client.uploadArtifact(
-        filenameWithoutExt,
-        [path.join(tempDir, artifactFilename)],
-        tempDir
-      );
+        await client.uploadArtifact(
+          artifactId,
+          [path.join(tempDir, artifactFilename)],
+          tempDir
+        );
 
-      core.info(`Uploaded ${artifactFilename} successfully`);
-    })
+        core.info(`Uploaded ${artifactFilename} successfully`);
+      })
   );
 }
 
