4a5d7a262dd32e37f569fd2f70bce427012a4c81
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2022-01-13 12:57:10 +0200

    use response data

--- artifactApi.js
+++ artifactApi.js
@@ -1,20 +1,25 @@
-const { Axios } = require('axios');
-const core = require('@actions/core');
+const { Axios } = require("axios");
+const core = require("@actions/core");
 
 class ArtifactApi {
   constructor() {
-    const repoToken = core.getInput('repo-token', { required: true, trimWhitespace: true });
+    const repoToken = core.getInput("repo-token", {
+      required: true,
+      trimWhitespace: true,
+    });
     this.axios = new Axios({
       baseURL: `https://api.github.com/repos/${process.env.GITHUB_REPOSITORY}`,
       headers: {
-        Accept: 'application/vnd.github.v3+json',
-        Authorization: `Bearer ${repoToken}`
+        Accept: "application/vnd.github.v3+json",
+        Authorization: `Bearer ${repoToken}`,
       },
     });
   }
 
   listArtifacts() {
-    return this.axios.get('/actions/artifacts', { per_page: 100 });
+    return this.axios
+      .get("/actions/artifacts", { per_page: 100 })
+      .then((response) => response.data);
   }
 }
 
